a205971193b4615e804bf1ba1db472aad7e932b6
Giovanni Battaglia <iamgiovanni.gb@gmail.com>
2024-06-02 23:01:19 +0200

    Tweaked Red Star Ring Graphics

--- _maps/Red Star Ring DPLCs.asm
+++ _maps/Red Star Ring DPLCs.asm
@@ -1,23 +1,23 @@
-DPLC_0354: mappingsTable
-	mappingsTableEntry.w	DPLC_0354_0
-	mappingsTableEntry.w	DPLC_0354_1
-	mappingsTableEntry.w	DPLC_0354_2
-	mappingsTableEntry.w	DPLC_0354_3
+DPLC_4f76: mappingsTable
+	mappingsTableEntry.w	DPLC_4f76_0
+	mappingsTableEntry.w	DPLC_4f76_1
+	mappingsTableEntry.w	DPLC_4f76_2
+	mappingsTableEntry.w	DPLC_4f76_3
 
-DPLC_0354_0:	dplcHeader
- dplcEntry $10, 0
-DPLC_0354_0_End
+DPLC_4f76_0:	dplcHeader
+ dplcEntry $10, $24
+DPLC_4f76_0_End
 
-DPLC_0354_1:	dplcHeader
- dplcEntry $C, $10
-DPLC_0354_1_End
+DPLC_4f76_1:	dplcHeader
+ dplcEntry $C, $34
+DPLC_4f76_1_End
 
-DPLC_0354_2:	dplcHeader
- dplcEntry 8, $1C
-DPLC_0354_2_End
+DPLC_4f76_2:	dplcHeader
+ dplcEntry 8, $40
+DPLC_4f76_2_End
 
-DPLC_0354_3:	dplcHeader
- dplcEntry $C, $10
-DPLC_0354_3_End
+DPLC_4f76_3:	dplcHeader
+ dplcEntry $C, $34
+DPLC_4f76_3_End
 
 	even
--- _maps/Red Star Ring.asm
+++ _maps/Red Star Ring.asm
@@ -1,23 +1,23 @@
-Map_ea03: mappingsTable
-	mappingsTableEntry.w	Map_ea03_0
-	mappingsTableEntry.w	Map_ea03_1
-	mappingsTableEntry.w	Map_ea03_2
-	mappingsTableEntry.w	Map_ea03_3
+Map_854f: mappingsTable
+	mappingsTableEntry.w	Map_854f_0
+	mappingsTableEntry.w	Map_854f_1
+	mappingsTableEntry.w	Map_854f_2
+	mappingsTableEntry.w	Map_854f_3
 
-Map_ea03_0:	spriteHeader
+Map_854f_0:	spriteHeader
  spritePiece -$10, -$10, 4, 4, 0, 0, 0, 0, 0
-Map_ea03_0_End
+Map_854f_0_End
 
-Map_ea03_1:	spriteHeader
+Map_854f_1:	spriteHeader
  spritePiece -$C, -$10, 3, 4, 0, 0, 0, 0, 0
-Map_ea03_1_End
+Map_854f_1_End
 
-Map_ea03_2:	spriteHeader
- spritePiece -6, -$10, 2, 4, 0, 0, 0, 0, 0
-Map_ea03_2_End
+Map_854f_2:	spriteHeader
+ spritePiece -5, -$10, 2, 4, 0, 0, 0, 0, 0
+Map_854f_2_End
 
-Map_ea03_3:	spriteHeader
- spritePiece -$C, -$10, 3, 4, 0, 1, 0, 0, 0
-Map_ea03_3_End
+Map_854f_3:	spriteHeader
+ spritePiece -$B, -$10, 3, 4, 0, 1, 0, 0, 0
+Map_854f_3_End
 
 	even
