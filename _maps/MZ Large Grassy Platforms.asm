3869897fd721771de8ad224fe1bf98c2156090d3
Giovanni Battaglia <iamgiovanni.gb@gmail.com>
2024-07-14 14:36:51 +0200

    the miracle of right facing slope

--- _maps/MZ Large Grassy Platforms.asm
+++ _maps/MZ Large Grassy Platforms.asm
@@ -1,40 +1,42 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - large moving grass-covered platforms (MZ)
-; ---------------------------------------------------------------------------
-Map_LGrass_internal:
-		dc.w .wide-Map_LGrass_internal
-		dc.w .sloped-Map_LGrass_internal
-		dc.w .narrow-Map_LGrass_internal
-.wide:		dc.b $D
-		dc.b $D8, 6, 0,	$57, $C0 ; wide platform
-		dc.b $F0, 5, 0,	$53, $C0
-		dc.b 0,	$F, 0, 1, $C0
-		dc.b $D0, $F, 0, $27, $D0
-		dc.b $F0, $D, 0, $37, $D0
-		dc.b $F0, $F, 0, 1, $E0
-		dc.b $D0, $F, 0, $11, $F0
-		dc.b $D0, $F, 0, $3F, $10
-		dc.b $F0, $D, 0, $4F, $10
-		dc.b $F0, $F, 0, 1, 0
-		dc.b 0,	$F, 0, 1, $20
-		dc.b $D8, 6, 0,	$57, $30
-		dc.b $F0, 5, 0,	$53, $30
-.sloped:	dc.b $A
-		dc.b $D0, $F, 0, $27, $C0 ; sloped platform (catches fire)
-		dc.b $F0, $D, 0, $37, $C0
-		dc.b 0,	$F, 0, 1, $C0
-		dc.b $C0, $F, 0, $27, $E0
-		dc.b $E0, $D, 0, $37, $E0
-		dc.b $F0, $F, 0, 1, $E0
-		dc.b $C0, $F, 0, $11, 0
-		dc.b $E0, $F, 0, 1, 0
-		dc.b $C0, $F, 0, $3F, $20
-		dc.b $E0, $D, 0, $4F, $20
-.narrow:	dc.b 6
-		dc.b $D0, $F, 0, $11, $E0 ; narrow platform
-		dc.b $F0, $F, 0, 1, $E0
-		dc.b $10, $F, 0, 1, $E0
-		dc.b $D0, $F, 0, $11, 0
-		dc.b $F0, $F, 0, 1, 0
-		dc.b $10, $F, 0, 1, 0
-		even
+Map_6aa3:
+Map_6aa3_0: 	dc.w Map_6aa3_6-Map_6aa3
+Map_6aa3_2: 	dc.w Map_6aa3_52-Map_6aa3
+Map_6aa3_4: 	dc.w Map_6aa3_94-Map_6aa3
+Map_6aa3_6: 	dc.b $F
+	dc.b $D8, $6, $0, $57, $C0
+	dc.b $F0, $5, $0, $53, $C0
+	dc.b $0, $F, $0, $1, $C0
+	dc.b $F0, $D, $0, $37, $D0
+	dc.b $F0, $F, $0, $1, $E0
+	dc.b $D0, $F, $0, $11, $F0
+	dc.b $F0, $D, $0, $4F, $10
+	dc.b $F0, $F, $0, $1, $0
+	dc.b $0, $F, $0, $1, $20
+	dc.b $D8, $6, $0, $57, $30
+	dc.b $F0, $5, $0, $53, $30
+	dc.b $D8, $2, $0, $28, $D0
+	dc.b $D0, $B, $0, $2B, $D8
+	dc.b $D8, $2, $0, $4C, $28
+	dc.b $D0, $B, $0, $3F, $10
+Map_6aa3_52: 	dc.b $D
+	dc.b $F0, $D, $0, $37, $C0
+	dc.b $0, $F, $0, $1, $C0
+	dc.b $E0, $D, $0, $37, $E0
+	dc.b $F0, $F, $0, $1, $E0
+	dc.b $C0, $F, $0, $11, $0
+	dc.b $E0, $F, $0, $1, $0
+	dc.b $E0, $D, $0, $4F, $20
+	dc.b $D8, $2, $0, $28, $C0
+	dc.b $D0, $B, $0, $2B, $C8
+	dc.b $C8, $2, $0, $28, $E0
+	dc.b $C0, $B, $0, $2B, $E8
+	dc.b $C0, $B, $0, $3F, $20
+	dc.b $C8, $2, $0, $4C, $38
+Map_6aa3_94: 	dc.b $6
+	dc.b $D0, $F, $0, $11, $E0
+	dc.b $F0, $F, $0, $1, $E0
+	dc.b $10, $F, $0, $1, $E0
+	dc.b $D0, $F, $0, $11, $0
+	dc.b $F0, $F, $0, $1, $0
+	dc.b $10, $F, $0, $1, $0
+	even
